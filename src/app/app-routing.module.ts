4cd3f4c99e8687edcd6d95575df7a4f3fc654019
billsplitgit <billsplitgit@gmail.com>
2017-09-22 15:09:37 +0530

    first

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HeroListComponent } from "./hero-list/hero-list.component";
 
 const routes: Routes = [
-  {
-    path: '',
-    children: []
-  }
-];
+  {path: '', pathMatch: 'full', redirectTo: 'AppComponent'},
+  {path: 'list', component: HeroListComponent}
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/hero-list/hero-list.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeroListComponent } from './hero-list.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [HeroListComponent]
+})
+export class HeroListModule { }
